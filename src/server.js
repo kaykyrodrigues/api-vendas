82b0f63821a36ad968b4f31bae4b09761965101b
Kayky <rodrigueskayky2025@gmail.com>
2026-05-13 16:15:01 -0300

    Adicionando script start

--- src/server.js
+++ src/server.js
@@ -4,6 +4,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3036;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
-
